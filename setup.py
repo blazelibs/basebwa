5e0520ab185fc4bccb29aba22e16a28a6b1846bb
Randy Syring <randy.syring@level12.io>
2008-12-16 15:29:56 +0000

    adding package_data

--- setup.py
+++ setup.py
@@ -1,3 +1,55 @@
+"""
+Introduction
+---------------
+
+pysapp is a library designed as a "supporting application" for
+`pysmvt <http://pypi.python.org/pypi/pysmvt/>`_ applications.
+
+Documentation and example applications are not yet available.
+
+Steps for Installation
+----------------------
+
+#. Install Python
+#. install setuptools (includes easy_install)
+#. install virtualenv `easy_install virtualenv`
+#. make a staging area `mkdir myproj-staging`
+#. `cd myproj-staging`
+#. Create a new virtual environement `virtualenv ENV --no-site-packages`
+#. `Activate the virtual environment <http://pypi.python.org/pypi/virtualenv#activate-script>`
+#. install pysapp & dependencies `easy_install pysapp`
+
+Steps for creating a working application
+-----------------------------------------
+
+#. `pysmvt project -t pysapp <myapp>` replace <myapp> with your project name
+#. answer the questions that come up
+#. when prompted to overwrite, type `all y`
+#. write down "Login Details", referred to hereafter as <user> & <pass>
+#. `cd <myapp>-dist`
+#. `python setup.py -q develop`
+#. `cd <myapp>`
+#. `pysmvt broadcast initdb` setup the database tables
+#. `pysmvt broadcast initmod -p <user>` let modules set themselves up, use the 
+   settings profile for your user
+#. `pysmvt serve <user>` run a development http server with the user's settings 
+   profile
+    
+Creating a new application module
+---------------------------------
+`pysmvt module <mymod>` where <mymod> is the name of the module you want to create
+
+
+Current Status
+---------------
+
+We are currently in an alpha phase which means lots of stuff can change, maybe rapidly, and we are not interested in backwards compatibility at this point.
+
+I am currently using this library for some production websites, but I wouldn't recommend you do that unless you **really** know what you are doing.
+
+The unstable `development version
+<https://svn.rcslocal.com:8443/svn/pysmvt/pysapp/trunk/#egg=pysapp-dev>`_.
+"""
 import sys
 try:
     from setuptools import setup, find_packages
@@ -8,28 +60,24 @@ except ImportError:
 
 setup(
     name = "pysapp",
-    version = "0.1.0",
+    version = "0.1dev",
     description = "A pysmvt supporting application",
-    long_description = "Coming eventually",
+    long_description = __doc__,
     author = "Randy Syring",
     author_email = "randy@rcs-comp.com",
-    url='http://pypi.python.org/pypi/pysapp',
+    url='http://pypi.python.org/pypi/pysapp/',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
       ],
     license='BSD',
-    packages=['pysapp'],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
     install_requires = [
         "Elixir>=0.6.1",
         "pysmvt>=dev",
-        "pysform>=dev",
         "SQLiteFKTG4SA>=dev",
-    ],
-    dependency_links = [
-        "https://svn.rcslocal.com:8443/svn/pysmvt/pysmvt/trunk/#egg=pysmvt-dev",
-        "https://svn.rcslocal.com:8443/svn/pysmvt/pysform/trunk/#egg=pysform-dev"
     ],
     entry_points="""
     [pysmvt.pysmvt_project_template]
