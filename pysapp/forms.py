1b348c0dc41bd533aff4a9ee2d8f604c728413c9
Randy Syring <randy.syring@level12.io>
2008-12-31 02:29:20 +0000

    pysapp/forms.py: removed reference to pyhtmlquickform

--- pysapp/forms.py
+++ pysapp/forms.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-from pyhtmlquickform.form import Form as QForm
 from pysmvt import rg, user
 from pysform.form import Form as Pysform
-
-class QuickFormBase(QForm):
-
-    def __init__(self, name, **kwargs):
-        QForm.__init__(self, name, class_='generated', **kwargs)
-        self._request_submitted = False
-        
-    def isSubmitted(self, submitValues = None):
-        # don't want to repeat this and isSubmitted can be used
-        # more than once
-        if not self._request_submitted:
-            self.set_submitted(rg.request.form)
-            self.set_files(rg.request.files)
-            self._request_submitted = True
-        return QForm.isSubmitted(self, submitValues)
 
 class Form(Pysform):
     def __init__(self, name, **kwargs):
