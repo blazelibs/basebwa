cef85c9b1acb98b507e09655b0724b1f9cc28278
Randy Syring <randy.syring@level12.io>
2009-06-30 15:11:15 +0000

    preliminary work for datagrid module

---
+++ pysapp/modules/datagrid/tests/test_query_building.py
@@ -0,0 +1,12 @@
+from pysmvt import db
+from pysmvt.routing import current_url
+from pysmvt.utils import wrapinapp
+from pysapp.modules.datagrid.utils import DataGrid, DataColumn, TableColumn
+#from pysapptestapp.applications import make_test_app
+
+#app = make_test_app()
+
+class TestQueryBuilding(object):
+    
+    def test_default(self):
+        pass
