19df222ed330c502fe777212c2a452b2a4748dcc
Randy Syring <randy.syring@level12.io>
2009-07-06 19:10:47 +0000

    pysapp/utils.py: added run_module_sql() as an easy way of getting SQL statements into the DB (i.e. creating views).  Used for an organizations view in the FolioFund project.

--- pysapp/utils.py
+++ pysapp/utils.py
@@ -38,3 +38,45 @@ class ControlPanelLink(object):
     def __init__(self, text, endpoint):
         self.text = text
         self.endpoint = endpoint
+
+def run_module_sql(module, target, use_dialect=False):
+    ''' used to run SQL from files in a modules "sql" directory:
+    
+            run_module_sql('mymod', 'create_views')
+        
+        will run the file "<myapp>/modules/mymod/sql/create_views.sql"
+        
+            run_module_sql('mymod', 'create_views', True)
+        
+        will run the files:
+            
+            # sqlite DB
+            <myapp>/modules/mymod/sql/create_views.sqlite.sql
+            # postgres DB
+            <myapp>/modules/mymod/sql/create_views.pgsql.sql
+            ...
+        
+        The dialect prefix used is the same as the sqlalchemy prefix.
+        
+        The SQL file can contain multiple statements.  They should be seperated
+        with the text "--statement-break".
+            
+    '''
+    from pysmvt import db, appfilepath
+    from os.path import join
+
+    if use_dialect:
+        relative_sql_path = 'modules/%s/sql/%s.%s.sql' % (module, target, db.engine.dialect.name )
+    else:
+        relative_sql_path = 'modules/%s/sql/%s.sql' % (module, target )
+    
+    full_path = appfilepath(relative_sql_path)
+    
+    sqlfile = file(full_path)
+    sql = sqlfile.read()
+    sqlfile.close()
+    for statement in sql.split('--statement-break'):
+        statement.strip()
+        if statement:
+            db.sess.execute(statement)
+    db.sess.commit()
