185a82c5bd77538f10e469bc07857981fc0dd275
Randy Syring <randy.syring@level12.io>
2009-09-05 03:36:40 +0000

    - implementing new method for assuring setup_db_structure() gets called by nose instead of needing to do it manually

--- pysappexample-dist/pysappexample/__init__.py
+++
@@ -1,8 +0,0 @@
-from pysmvt import appimport
-
-# Don't use global dependant objects here.  This code will get ran before the
-# WSGI application gets setup.
-
-def setup_package():
-    setup_db_structure = appimport('testing', 'setup_db_structure')
-    setup_db_structure()
