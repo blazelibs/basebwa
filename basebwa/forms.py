e29718a5ad137d678fb459526dade89a18514819
Matt Lewellyn <guruofgentoo@users.noreply.github.com>
2010-06-23 11:40:50 -0400

    removed code that was moved over to SQLAlchemyBWP

---
+++ basebwa/forms.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from blazeweb.globals import rg, user
+from blazeform.form import Form as blazeform
+from blazeweb.routing import current_url
+from blazeform.util import NotGiven
+from formencode.validators import FancyValidator
+from formencode import Invalid
+from blazeweb.utils import registry_has_object, werkzeug_multi_dict_conv
+
+class Form(blazeform):
+    note_prefix = '- '
+    error_prefix = '- '
+    req_note_level = 'section'
+    req_note = None
+
+    def __init__(self, name, **kwargs):
+        if registry_has_object(rg):
+            curl = current_url(strip_host=True)
+        else:
+            curl = ''
+        action = kwargs.pop('action', curl)
+        class_ = kwargs.pop('class_', NotGiven)
+        if class_ is NotGiven:
+            kwargs['class_'] = 'generated'
+        elif class_:
+            kwargs['class_'] = class_
+        blazeform.__init__(self, name, action=action, **kwargs)
+        self._request_submitted = False
+
+    def assign_from_request(self, req):
+        to_submit = werkzeug_multi_dict_conv(req.form)
+        to_submit.update(req.files.to_dict())
+        self.set_submitted(to_submit)
+
+    def is_submitted(self):
+        # don't want to repeat the assignment and is_submitted might be used
+        # more than once
+        if not self._request_submitted and not self._static:
+            if registry_has_object(rg):
+                self.assign_from_request(rg.request)
+            self._request_submitted = True
+        return blazeform.is_submitted(self)
+
+    def assign_user_errors(self):
+        # set the form error messages first
+        for msg in self._errors:
+            user.add_message('error', msg)
+        # set element error messages
+        for el in self.submittable_els:
+            for msg in el.errors:
+                user.add_message('error', '%s: %s' % (el.label, msg))
+
+    def render(self, **kwargs):
+        if self.note_prefix:
+            kwargs.setdefault('note_prefix', self.note_prefix)
+        if self.error_prefix:
+            kwargs.setdefault('error_prefix', self.error_prefix)
+        if self.req_note_level:
+            kwargs.setdefault('req_note_level', self.req_note_level)
+        if self.req_note:
+            kwargs.setdefault('req_note', self.req_note)
+        return blazeform.render(self, **kwargs)
+
+class UniqueValidator(FancyValidator):
+    """
+    Calls the given callable with the value of the field.  If the return value
+    does not evaluate to false, Invalid is raised
+    """
+
+    __unpackargs__ = ('fn')
+    messages = {
+        'notunique': "the value for this field must be unique",
+        }
+
+    def validate_python(self, value, state):
+        if value == state.defaultval:
+            return
+        if self.fn(value):
+            raise Invalid(self.message('notunique', state), value, state)
+        return
---
+++ setup.py
@@ -0,0 +1,107 @@
+"""
+Introduction
+---------------
+
+basebwa is a library designed as a "supporting application" for
+`blazeweb <http://pypi.python.org/pypi/blazeweb/>`_ applications.
+
+Example applications will be avilable soon, for now, please follow the steps
+below to get your own default application up and running.
+
+Steps for Installation
+----------------------
+
+#. Install Python
+#. install setuptools (includes easy_install)
+#. install virtualenv `easy_install virtualenv`
+#. Create a new virtual environement `virtualenv myproj-staging --no-site-packages`
+#. `Activate the virtual environment (os dependent) <http://pypi.python.org/pypi/virtualenv#activate-script>`_
+#. install basebwa & dependencies `easy_install basebwa`
+
+Steps for creating a working application
+-----------------------------------------
+
+Note: the `blazeweb` command is installed when you install basebwa, which has
+blazeweb as a requirement.
+
+#. `cd myproj-staging`
+#. `mkdir src`
+#. `cd src`
+#. `blazeweb project -t basebwa <myapp>` replace <myapp> with your project name
+#. answer the questions that come up
+#. write down "Login Details", referred to hereafter as <user> & <pass>
+#. `cd <myapp>-dist`
+#. `python setup.py -q develop`
+#. `cd <myapp>`
+#. `blazeweb broadcast initdb` setup the database tables
+#. `blazeweb broadcast initdata <user>` put basic data in the database, use the
+   settings profile for your user
+#. `blazeweb serve <user>` run a development http server with the user's settings
+   profile
+#. point your browser at http://localhost:5000/
+
+Creating a New Application Module
+---------------------------------
+This step creates a Application Module directory structure in <myapp>/modules/<mymod>:
+
+`blazeweb module <mymod>`
+
+where <mymod> is the name of the module you want to create
+
+Questions & Comments
+---------------------
+
+Please visit: http://groups.google.com/group/pyslibs
+
+Current Status
+---------------
+
+The code stays pretty stable, but the API is likely to change in the future.
+
+The `basebwa tip <http://bitbucket.org/rsyring/basebwa/get/tip.zip#egg=basebwa-dev>`_
+is installable via `easy_install` with ``easy_install basebwa==dev``
+"""
+import sys
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+version = '0.2.1'
+
+setup(
+    name = "BaseBWA",
+    version = version,
+    description = "A blazeweb supporting application",
+    long_description = __doc__,
+    author = "Randy Syring",
+    author_email = "rsyring@gmail.com",
+    url='http://pypi.python.org/pypi/basebwa/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+      ],
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'tests']),
+    include_package_data=True,
+    install_requires = [
+        "BlazeWeb>=0.2",
+        "BlazeForm>=0.2",
+        "SQLiteFKTG4SA>=0.1.1",
+        "python-dateutil>=1.4.1"
+    ],
+    entry_points="""
+    [blazeweb.blazeweb_project_template]
+    basebwa = basebwa.lib.paster_tpl:ProjectTemplate
+
+    [blazeweb.blazeweb_module_template]
+    basebwa = basebwa.lib.paster_tpl:ModuleTemplate
+
+    [console_scripts]
+    basebwa = basebwa.application:script_entry
+    """,
+    zip_safe=False
+)
