411b2462fe36b3cd4564a267b88d2b45c89f6844
Randy Syring <randy.syring@level12.io>
2010-07-12 11:07:59 -0400

    removing files that are no longer needed

--- basebwa/commands.py
+++
@@ -1,8 +0,0 @@
-from blazeweb.globals import commands as cmds
-
-def action_module(modname='', template=('t', 'basebwa'),
-        interactive=True, verbose=False, overwrite=True):
-    """ creates a new module file structure (basebwa default)"""
-    cmds.action_module(modname, template, interactive, verbose, overwrite)
-
-    
