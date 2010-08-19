65703ff4021b167cc541d88856e07885ffee007e
Randy Syring <randy.syring@level12.io>
2010-08-19 17:24:36 -0400

    apputil: remove fatal dependency on auth plugin
    for paicore, I didn't want the auth plugin and when I didn't include
    	it then the apputil init_db task failed.  Don't really like
    	this solution, but it works and I want to get rid of apputil
    	anyway eventually.

--- basebwa/plugins/apputil/tasks/init_db.py
+++ basebwa/plugins/apputil/tasks/init_db.py
@@ -1,6 +1,12 @@
 from blazeweb.tasks import attributes
 
-from plugstack.auth.model.actions import permission_update
+try:
+    # if the auth module is not available, then give a placeholder function
+    # to avoid the exception
+    from plugstack.auth.model.actions import permission_update
+except ImportError:
+    def permission_update(*args, **kwargs):
+        pass
 
 @attributes('base-data')
 def action_30_base_data():
